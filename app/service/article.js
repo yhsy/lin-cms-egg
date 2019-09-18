454692a9e1665ef3bf76029726954f9f8a8cf92e
fishku <juerui@vip.qq.com>
2019-09-18 22:23:58 +0800

    文章管理-列表页-接口优化(结束日期统一加一天)

--- app/service/article.js
+++ app/service/article.js
@@ -20,7 +20,7 @@ const moment = require('moment');
 
 class ArticleService extends Service {
   // 添加文章
-  async add() {
+  async add () {
     const { ctx } = this;
     const requestObj = ctx.request.body;
     const result = await ctx.model.Article.create(requestObj);
@@ -28,13 +28,13 @@ class ArticleService extends Service {
     return result;
   }
   // 获取文章详情
-  async info(id) {
+  async info (id) {
     const { ctx } = this;
     const result = await ctx.model.Article.findOne({ where: { id } });
     return result;
   }
   // 编辑文章
-  async edit(id) {
+  async edit (id) {
     const { ctx } = this;
     const requestObj = ctx.request.body;
     // 方法1
@@ -44,7 +44,7 @@ class ArticleService extends Service {
     return result;
   }
   // 删除文章(软删除)
-  async del(id) {
+  async del (id) {
     const { ctx } = this;
     const requestObj = {
       is_delete: 1,
@@ -53,14 +53,14 @@ class ArticleService extends Service {
     return result;
   }
   // 删除文章(硬删除-数据库直接删除)
-  async remove(id) {
+  async remove (id) {
     const { ctx } = this;
     const result = await ctx.model.Article.destroy({ where: { id } });
     return result;
   }
 
   // 文章列表
-  async list() {
+  async list () {
     const { ctx } = this;
     const requestObj = ctx.request.query;
     const { page, limit, startTime, endTime } = ctx.request.query;
@@ -93,33 +93,22 @@ class ArticleService extends Service {
     // 浏览量
     // 创建时间
     if (startTime && endTime) {
-      if (startTime === endTime) {
-        // console.log('相等');
-        whereObj.created_at = {
-          // 大于等于
-          [Op.gte]: startTime,
-          // 小于
-          [Op.lte]: moment(endTime).add(1, 'days'),
-        };
-      } else {
-        whereObj.created_at = {
+      whereObj.created_at = {
         // 大于等于
-          [Op.gte]: startTime,
-          // 小于
-          [Op.lte]: endTime,
-        };
-      }
-
+        [Op.gte]: startTime,
+        // 小于等于
+        [Op.lte]: moment(endTime).add(1, 'days'),
+      };
     }
 
     // 文章列表(分页)
     const list = await ctx.model.Article.findAll({
       where: whereObj,
       // 返回过滤字段(浏览量和软删除)
-      attributes: { exclude: [ 'pageviews', 'is_delete' ] },
+      attributes: { exclude: ['pageviews', 'is_delete'] },
       order: [
         // 创建时间-倒序
-        [ 'created_at', 'DESC' ],
+        ['created_at', 'DESC'],
         // [ 'id', 'DESC' ],
       ],
       // 条数
